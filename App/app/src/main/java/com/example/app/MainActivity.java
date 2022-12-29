1e170ba0df8e5599a5053bc82d0036dabff627f1
Florentin <florentin.bgn@outlook.fr>
2022-12-29 18:37:20 +0100

    Transition d'un affichage en liste à un affichage en grille et inversement

--- App/app/src/main/java/com/example/app/MainActivity.java
+++ App/app/src/main/java/com/example/app/MainActivity.java
@@ -7,6 +7,7 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,5 +42,25 @@ public class MainActivity extends AppCompatActivity {
                 return 10;
             }
         });
+
+
+
+        SwitchCompat switchCompat = findViewById(R.id.switch1);
+        switchCompat.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            viewPager2.setAdapter(new FragmentStateAdapter(this) {
+                @NonNull
+                @Override
+                public Fragment createFragment(int position) {
+                    return  ItemFragment.newInstance( isChecked ? 1 : 2 );
+                }
+
+                @Override
+                public int getItemCount() {
+                    return 10;
+                }
+            });
+        });
+
+
     }
 }
